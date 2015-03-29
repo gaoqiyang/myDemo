7f488bfc2c1e5a3de2bc8c85bc7b9eda03aeb429
gaoqiyang <qianfeng@gaoqiyang.local>
2015-03-29 18:03:51 +0800

    Initial Commit

---
+++ myDemo123/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  myDemo123
+//
+//  Created by qianfeng on 15/3/29.
+//  Copyright (c) 2015年 gaoqiyang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ myDemo123/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  myDemo123
+//
+//  Created by qianfeng on 15/3/29.
+//  Copyright (c) 2015年 gaoqiyang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
